22b2abaaaf918f184288aeea7cb8f39ef580f495
Nad795 <nadineirawan2005@gmail.com>
2024-12-02 15:06:16 +0700

    tak jadiin pbo sek

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State { FreeRoam, Dialog, Battle }
+public class Main : MonoBehaviour
+{
+    [SerializeField] Player player;
+    
+    State state;
+
+    private void Start()
+    {
+        DialogManager.Instance.OnShowDialog += () =>
+        {
+            state = State.Dialog;
+        };
+
+        DialogManager.Instance.OnHideDialog += () =>
+        {
+            if(state == State.Dialog)
+            {
+                state = State.FreeRoam;
+            }
+        };
+    }
+
+    private void Update()
+    {
+        if(state == State.FreeRoam)
+        {
+            player.HandleUpdate();
+
+        }
+        else if(state == State.Dialog)
+        {
+            DialogManager.Instance.HandleUpdate();
+        }
+        else if(state == State.Battle)
+        {
+
+        }
+    }
+}
